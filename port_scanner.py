6b4cf3e584984f3bf902b11bd4b922e24f7d374a
Aleksa <fankerman11@gmail.com>
2023-02-25 19:09:28 +0100

    Initial commit.

---
+++ port_scanner.py
@@ -0,0 +1,61 @@
+import socket
+import sys
+import time
+import threading
+import os
+
+from datetime import datetime
+
+print(" _______  _______ _________ _______  _______\n"
+      "(  ___  )(  ____ \\__   __/(  ____ )(  ___  )\n"
+      "| (   ) || (    \/   ) (   | (    )|| (   ) |\n"
+      "| (___) || (_____    | |   | (____)|| (___) |\n"
+      "|  ___  |(_____  )   | |   |     __)|  ___  |\n"
+      "| (   ) |      ) |   | |   | (\ (   | (   ) |\n"
+      "| )   ( |/\____) |   | |   | ) \ \__| )   ( |\n"
+      "|/     \|\_______)   )_(   |/   \__/|/     \|\n\n")
+
+
+IP = input("Enter IP Address: ")
+
+# Banner
+print("-" * 50)
+print("Scanning IP Address: " + IP)
+print("Scanning started at: " + str(datetime.now()))
+print("-" * 50)
+
+ports = []
+
+# Script for detecting all of the open ports on the server(IP)
+def scan(port):
+    # Create sockets   
+    connection = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    socket.setdefaulttimeout(1)
+    try:
+        # Return open ports
+        connection.connect((IP, port))
+        connection.close()
+        print(f"[*] ~ Port [ {port} ] is open.")
+        ports.append(port)
+    except Exception:
+        pass
+    except KeyboardInterrupt:
+        print("Exiting...")
+        sys.exit()
+
+try:
+    scanned = 1
+    # Scan every port on the server 
+    for port in range(1, 65500): # Valid port range for all systems
+        thread = threading.Thread(target=scan, kwargs={'port':scanned})
+        scanned += 1
+        thread.start()
+        # time.sleep(0.0001)
+except KeyboardInterrupt:
+    print("\n[*] ~ Exiting...")
+    sys.exit()
+    
+print("-" * 50)
+print(f"{scanned} ports were scanned.")
+print("Open ports: " + str(ports))
+    
