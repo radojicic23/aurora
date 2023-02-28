81d243fd3d324b56f0a0f79099ecf5cec3ea1143
Aleksa <fankerman11@gmail.com>
2023-02-28 15:03:57 +0100

    GUI changes.

--- lan_scanner.py
+++ lan_scanner.py
@@ -7,9 +7,9 @@ ip_range_pattern = re.compile("^(?:[0-9]{1,3}\.){3}[0-9]{1,3}/[0-9]*$")
 
 # Get the address range to ARP
 while True:
-    ip_entered_pattern = input("\nEnter IP Address and Range: ")
+    ip_entered_pattern = input("\n[*] ~ Enter IP Address and Range: ")
     if ip_range_pattern.search(ip_entered_pattern):
-        print(f"{ip_entered_pattern} is a valid ip address range.")
+        print(f"~ {ip_entered_pattern} is a valid ip address range.")
         break
 
 # The arping() method in scapy creates a pakcet with and ARP message
