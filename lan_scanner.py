74fc6eb7652b9347dda53e9cec7a6c721d238ee3
Aleksa <fankerman11@gmail.com>
2023-02-28 14:55:07 +0100

    Added script for scanning devices connected to the network.

---
+++ lan_scanner.py
@@ -0,0 +1,17 @@
+import scapy.all as scapy
+import re
+
+
+# Regular expression pattern to recognise IPv4 addresses
+ip_range_pattern = re.compile("^(?:[0-9]{1,3}\.){3}[0-9]{1,3}/[0-9]*$")
+
+# Get the address range to ARP
+while True:
+    ip_entered_pattern = input("\nEnter IP Address and Range: ")
+    if ip_range_pattern.search(ip_entered_pattern):
+        print(f"{ip_entered_pattern} is a valid ip address range.")
+        break
+
+# The arping() method in scapy creates a pakcet with and ARP message
+# and sens it to the broadcast mac address ff:ff:ff:ff:ff:ff
+arp_result = scapy.arping(ip_entered_pattern)
